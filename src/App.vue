421dbacd4bb1cc1294f2289c3c6f665c3f93449b
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2024-02-13 16:45:52 +0200

    SPA Add API proxy to Vite config

--- src/App.vue
+++ src/App.vue
@@ -25,19 +25,13 @@ watchEffect(async () => {
     }
   }`;
 
-  const fetchUrl = `https://graphql.contentful.com/content/v1/spaces/${import.meta.env.VITE_CONTENTFUL_SPACE_ID}`;
-
   const fetchOptions = {
         method: "POST",
-        headers: {
-          Authorization: `Bearer ${import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN}`,
-          "Content-Type": "application/json"
-        },
         body: JSON.stringify({ query })
       };
 
     try {
-        const response = await fetch(fetchUrl, fetchOptions).then(response =>
+        const response = await fetch(`/api`, fetchOptions).then(response =>
           response.json()
         );
         plants.value = response.data.plantCollection.items;
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,37 @@
 import { fileURLToPath, URL } from 'node:url'
+import process from 'node:process'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [
+      vue(),
+    ],
+    resolve: {
+      alias: {
+        '@': fileURLToPath(new URL('./src', import.meta.url))
+      }
+    },
+    server: {
+      proxy: {
+        // with options: http://localhost:5173/api-> https://graphql.contentful.com/content/v1/spaces/:space-id
+        '/api': {
+          target: `https://graphql.contentful.com/content/v1/spaces/${env.VITE_CONTENTFUL_SPACE_ID}`,
+          headers: {
+            Authorization: `Bearer ${env.VITE_CONTENTFUL_ACCESS_TOKEN}`,
+            "Content-Type": "application/json"
+          },
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+        },
+      },
+    },
   }
 })
