58b07ea4d9b8a28ae86bbe9ff767f55958a430c7
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2024-02-15 12:34:07 +0200

    SSR Add explanation comments to server.js

--- server.js
+++ server.js
@@ -21,11 +21,22 @@ const app = express()
 let vite
 if (!isProduction) {
   const { createServer } = await import('vite')
+
+  // Create Vite server in middleware mode and configure the app type as
+  // 'custom', disabling Vite's own HTML serving logic so parent server
+  // can take control
   vite = await createServer({
     server: { middlewareMode: true },
     appType: 'custom',
     base
   })
+
+  // Use vite's connect instance as middleware. If you use your own
+  // express router (express.Router()), you should use router.use
+  // When the server restarts (for example after the user modifies
+  // vite.config.js), `vite.middlewares` is still going to be the same
+  // reference (with a new internal stack of Vite and plugin-injected
+  // middlewares). The following is valid even after restarts.
   app.use(vite.middlewares)
 } else {
   const compression = (await import('compression')).default
@@ -43,22 +54,46 @@ app.use('*', async (req, res) => {
     let render
     if (!isProduction) {
       // Always read fresh template in development
+      // 1. Read index.html
       template = await fs.readFile('./index.html', 'utf-8')
+
+      // 2. Apply Vite HTML transforms. This injects the Vite HMR client,
+      //    and also applies HTML transforms from Vite plugins, e.g. global
+      //    preambles from @vitejs/plugin-react
       template = await vite.transformIndexHtml(url, template)
+
+      // 3a. Load the server entry. ssrLoadModule automatically transforms
+      //    ESM source code to be usable in Node.js! There is no bundling
+      //    required, and provides efficient invalidation similar to HMR.
       render = (await vite.ssrLoadModule('/src/entry-server.js')).render
+
+      // 3b. Since Vite 5.1, you can use createViteRuntime API instead.
+      //    It fully supports HMR and works in a simillar way to ssrLoadModule
+      //    More advanced use case would be creating a runtime in a separate
+      //    thread or even a different machine using ViteRuntime class
+
+      /* const runtime = await vite.createViteRuntime(server)
+      render = (await runtime.ssrLoadModule('/src/entry-server.js')).render */
     } else {
       template = templateHtml
       render = (await import('./dist/server/entry-server.js')).render
     }
 
+    // 4. render the app HTML. This assumes entry-server.js's exported
+    //    `render` function calls appropriate framework SSR APIs,
+    //    e.g. ReactDOMServer.renderToString()
     const rendered = await render(url, ssrManifest)
 
+    // 5. Inject the app-rendered HTML into the template.
     const html = template
       .replace(`<!--app-head-->`, rendered.head ?? '')
       .replace(`<!--app-html-->`, rendered.html ?? '')
 
+    // 6. Send the rendered HTML back.
     res.status(200).set({ 'Content-Type': 'text/html' }).send(html)
   } catch (e) {
+    // If an error is caught, let Vite fix the stack trace so it maps back
+    // to your actual source code.
     vite?.ssrFixStacktrace(e)
     console.log(e.stack)
     res.status(500).end(e.stack)
