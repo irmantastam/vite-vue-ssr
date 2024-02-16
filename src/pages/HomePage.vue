2d4016a3ba894427784f7c52fe7b75aa91d02476
Irmantas Tamašauskas <irmantas.tam@gmail.com>
2024-02-16 09:45:35 +0200

    SSR Implement API proxy

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -36,19 +36,15 @@ const fetchPlants = async () => {
     }
   }`
 
-  const fetchUrl = `https://graphql.contentful.com/content/v1/spaces/${import.meta.env.VITE_CONTENTFUL_SPACE_ID}`
-
   const fetchOptions = {
     method: 'POST',
-    headers: {
-      Authorization: `Bearer ${import.meta.env.VITE_CONTENTFUL_ACCESS_TOKEN}`,
-      'Content-Type': 'application/json'
-    },
     body: JSON.stringify({ query })
   }
 
   try {
-    const response = await fetch(fetchUrl, fetchOptions).then((response) => response.json())
+    const response = await fetch(import.meta.env.VITE_ROOT + '/api', fetchOptions).then(
+      (response) => response.json()
+    )
     return response?.data?.plantCollection?.items
   } catch (error) {
     throw new Error('Could not receive the data from Contentful!')
